cd9dbfa0624c8d559624109ae2ed3bbad194a18a
Emiliano-Murillo <emilianomurillo12@gmail.com>
2023-02-21 01:19:14 -0600

    Fix home page responsive layout

--- src/components/HomeSearch/index.js
+++ src/components/HomeSearch/index.js
@@ -29,9 +29,9 @@ export default function HomeSearch() {
   }
 
   return (
-    <form className='flex items-center' autoComplete='off' onSubmit={handleSubmit}>
+    <form className='flex items-center sm:flex-row flex-col gap-2 w-full' autoComplete='off' onSubmit={handleSubmit}>
       <label htmlFor='voice-search' className='sr-only'>Search</label>
-      <div className='relative w-full'>
+      <div className='relative w-full '>
         <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
           <svg aria-hidden='true' className='w-5 h-5 text-gray-500' fill='currentColor' viewBox='0 0 20 20'><path fillRule='evenodd' d='M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z' clipRule='evenodd' /></svg>
         </div>
@@ -47,11 +47,11 @@ export default function HomeSearch() {
       <Button
         type='submit'
         shadow
-        size='auto'
-        className='ml-2 py-2.5 h-[60px]'
+        size='sm'
+        className='sm:py-2.5 sm:h-[60px] w-[100%] flex-1'
         icon={<Search className='w-5 h-5' />}
       >
-        Search
+        Buscar
       </Button>
     </form>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       <Head>
         <title>YouFeel</title>
       </Head>
-      <main className='flex flex-col'>
+      <main className='flex flex-col relative'>
         <div className='home-bg-radial-gradient' />
         <div className='flex flex-col min-h-[calc(100vh-60px)]'>
           <header className='h-[80px] p-3 md:px-12 flex justify-end items-center'>
@@ -18,36 +18,36 @@ export default function Home() {
               <Button color='opaque' light rounded icon={<Github className='w-8 h-8' />} />
             </a>
           </header>
-          <section className='flex container self-center p-3 flex-auto flex-col justify-center items-center'>
-            <div className='max-w-lg w-full text-rose-400 mb-10'>
+          <section className='flex container self-center p-3 flex-auto flex-col justify-center items-center px-5'>
+            <div className='max-w-lg w-full text-rose-400 mb-10 px-4'>
               <BrandHorizontal />
             </div>
-            <div className='text-lg font-semibold mb-2 text-center'>
+            <div className='text-base sm:text-lg font-semibold mb-2 text-center'>
               YouTube Sentiment Analyzer
             </div>
-            <div className='text-center pb-4'>
+            <div className='text-xs sm:text-base text-center pb-4'>
               Observa lo que los usuarios piensan sobre cualquier contenido publicado en YouTube
             </div>
             <div className='max-w-2xl w-full mt-5 pb-6 flex flex-col'>
               <HomeSearch />
-              <span className='text-xs text-gray-400 mt-1'>
+              <span className='text-xs text-gray-400 mt-1 hidden sm:block'>
                 Es posible buscar un video en específico pegando la URL del video
               </span>
             </div>
             <div className='flex gap-3 mt-6 flex-wrap'>
-              <div className='px-4 py-4  border-2 rounded-lg text-sm hover:shadow-lg flex-1 md:flex-auto'>
+              <div className='px-4 py-4 border-2 rounded-lg text-sm hover:shadow-lg flex-1 md:flex-auto'>
                 Visualización de dislikes
               </div>
-              <div className='px-4 py-4  border-2 rounded-lg text-sm hover:shadow-lg flex-1 md:flex-auto'>
+              <div className='px-4 py-4 border-2 rounded-lg text-sm hover:shadow-lg flex-1 md:flex-auto'>
                 Clasificación de comentarios
               </div>
-              <div className='px-4 py-4  border-2 rounded-lg text-sm hover:shadow-lg flex-1 md:flex-auto'>
+              <div className='px-4 py-4 border-2 rounded-lg text-sm hover:shadow-lg flex-1 md:flex-auto hidden sm:block'>
                 Representaciones gráficas
               </div>
             </div>
           </section>
         </div>
-        <footer className='max-w-3xl px-3 w-full m-auto flex justify-between min-h-60px] flex-wrap gap-1'>
+        <footer className='max-w-3xl p-3 w-full m-auto flex justify-between min-h-60px] flex-wrap gap-1'>
           <Tag className='normal-case'>
             <a href='https://github.com/topics/midudev-cohere-2023' target='_blank' rel='noreferrer'>
               #midudev-cohere-2023
@@ -56,15 +56,17 @@ export default function Home() {
           <Tag className='normal-case'>
             <a href='https://github.com/emimudev/' target='_blank' rel='noreferrer'>developed by emimudev</a>
           </Tag>
-          <span className='text-sm'>
+          <span className='text-sm self-center m-auto sm:m-0'>
             Powered by <Tag className='normal-case'><a href='https://vercel.com/' target='_blank' rel='noreferrer'>Vercel</a></Tag> and <Tag className='normal-case'> <a href='https://cohere.ai/' target='_blank' rel='noreferrer'>co:here</a></Tag>
           </span>
         </footer>
         <style jsx>{`
           .home-bg-radial-gradient {
             position: absolute;
-            inset: 0;
             z-index: -1;
+            min-height: 100vh;
+            height: 100%;
+            width: 100%;
             background: radial-gradient(circle at 0% -10%, hsl(0deg 46% 90%), rgba(255, 255, 255, 0) 40%),
             radial-gradient(circle at 110% 50%, hsl(288, 100%, 95%), transparent 30%),
             radial-gradient(circle at 50% 120%, hsl(0deg 46% 90%), transparent 30%),
